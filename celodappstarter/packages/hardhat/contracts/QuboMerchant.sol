73d85fdef64081559cf68162ceda740fe0de87c2
Caleb <caleb@Calebs-MacBook-Pro.local>
2022-03-19 23:43:11 -0700

    QuboMerchant

---
+++ celodappstarter/packages/hardhat/contracts/QuboMerchant.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+
+contract QuboMerchant is Ownable, ReentrancyGuard {
+    address _CELO = 0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9;
+    bool _customerReceived = false;
+    bool _merchantSent = false;
+    string private _merchantName;
+
+    //Order ID = FileHash
+    mapping(uint256 => string) public orderList;
+    //Order ID = Boolean
+    mapping(uint256 => bool) public orderStatus;
+
+    constructor(string memory merchantName) {
+        // require(merchantName =! "", "Merchant name is blank");
+        _merchantName = merchantName;
+    }
+
+    function addPurchase(uint256 payableAmount) public payable nonReentrant {
+        require(msg.value == payableAmount, "Payable is not valid!");
+    }
+
+    function balanceOf() public view returns (uint256){
+        return address(this).balance;
+    }
+}
